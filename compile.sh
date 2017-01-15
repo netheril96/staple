6063dd5ede252c2061c59bce2913e442d6a750c2
Siyuan Ren <netheril96@gmail.com>
2017-01-15 17:34:07 +0800

    Format cpp files

--- compile.sh
+++ compile.sh
@@ -1,2 +1,5 @@
-mkoctfile --mex -I/usr/local/include -L/usr/local/lib -lopencv_core -lopencv_imgproc mexResize.cpp MxArray.cpp
-mkoctfile --mex -DMATLAB_MEX_FILE=1 gradientMex.cpp 
+#!/bin/sh
+MEX=/Applications/MATLAB_R2016b.app/bin/mex
+$MEX -I/usr/local/include -L/usr/local/lib -lopencv_core -lopencv_imgproc mexResize.cpp MxArray.cpp
+$MEX gradientMex.cpp 
+
