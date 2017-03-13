af3e90e448c49f898c5a44a773faa21c4d250d6a
Siyuan Ren <netheril96@gmail.com>
2017-03-13 14:09:53 +0800

    Update gradientMex

--- compile.sh
+++ compile.sh
@@ -1,2 +1,4 @@
-mkoctfile --mex -I/usr/local/include -L/usr/local/lib -lopencv_core -lopencv_imgproc mexResize.cpp MxArray.cpp
-mkoctfile --mex -DMATLAB_MEX_FILE=1 gradientMex.cpp 
+#!/bin/sh
+MEX=/Applications/MATLAB_R2016b.app/bin/mex
+$MEX -I"$HOME/brew/include" -L"$HOME/brew/lib" -I/usr/local/include -L/usr/local/lib -lopencv_core -lopencv_imgproc mexResize.cpp MxArray.cpp
+$MEX -DMATLAB_MEX_FILE=1 gradientMex.cpp 
